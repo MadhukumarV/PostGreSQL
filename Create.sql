daba846e330fd8b300f531a6c01f8605be7541fa
MADHUKUMAR V <109059617+MadhukumarV@users.noreply.github.com>
2024-12-03 13:33:33 +0530

    Create Create.sql

---
+++ Create.sql
@@ -0,0 +1,11 @@
+-----databas creation---------
+Create database Training;
+use Training;
+
+---------Table creation----
+
+create table madhu (
+  name varchar ,
+  age int ,
+  gender varchar ,
+  email_id varchar );
